5ef70db42553d3b59922a7af4ed7489d78572e81
Yoke Cheng <18322535679@163.com>
2023-08-18 17:29:38 +0800

    [feat]完成leetcode计划第二期

--- leetcode/0058.Length-of-Last-Word/58.Length of Last Word.go
+++ leetcode/0058.Length-of-Last-Word/58.Length of Last Word.go
@@ -2,15 +2,24 @@ package leetcode
 
 func lengthOfLastWord(s string) int {
 	last := len(s) - 1
+
+	// 从字符串末尾开始，跳过末尾的空格
 	for last >= 0 && s[last] == ' ' {
 		last--
 	}
+
+	// 如果字符串全为空格，则返回长度为 0
 	if last < 0 {
 		return 0
 	}
+
 	first := last
+
+	// 从末尾的空格处向前查找，找到最后一个单词的起始位置
 	for first >= 0 && s[first] != ' ' {
 		first--
 	}
+
+	// 返回最后一个单词的长度
 	return last - first
 }
