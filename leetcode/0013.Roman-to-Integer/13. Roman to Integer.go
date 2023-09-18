842791a801db7bb69416121ab56033d95934b119
Yoke Cheng <18322535679@163.com>
2023-09-18 16:20:52 +0800

    [feat]完成leetcode计划第二期

--- leetcode/0013.Roman-to-Integer/13. Roman to Integer.go
+++ leetcode/0013.Roman-to-Integer/13. Roman to Integer.go
@@ -1,33 +1,23 @@
 package leetcode
 
-var roman = map[string]int{
-	"I": 1,
-	"V": 5,
-	"X": 10,
-	"L": 50,
-	"C": 100,
-	"D": 500,
-	"M": 1000,
+var roman = map[byte]int{
+	'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
 }
 
 func romanToInt(s string) int {
-	if s == "" {
-		return 0
-	}
+	total := 0 // 初始化总数
 
-	num, lastInt, total := 0, 0, 0 // 初始化变量
+	prev := 0 // 初始化前一个字符的值为0
 
 	for i := len(s) - 1; i >= 0; i-- { // 反向遍历字符串
-		char := s[i : i+1] // 获取当前字符
-		num = roman[char]  // 查找字符对应的整数值
+		current := roman[s[i]] // 获取当前字符对应的整数值
 
-		if num < lastInt { // 当前数字小于上一个数字，需要进行减法运算
-			total -= num
-		} else { // 当前数字大于等于上一个数字，进行加法运算
-			total += num
+		if current >= prev { // 如果当前数字大于前一个数字
+			total += current // 需要加上前一个数字
+		} else {
+			total -= current // 否则减去当前数字
 		}
-
-		lastInt = num // 更新上一个数字
+		prev = current
 	}
 
 	return total
