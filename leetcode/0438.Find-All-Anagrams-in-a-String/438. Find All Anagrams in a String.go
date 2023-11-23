13c3853f9a9e4ef5ded938df957db2c4a250d0aa
Yoke Cheng <18322535679@163.com>
2023-11-23 17:23:40 +0800

    [feat]labuladong 的算法笔记

--- leetcode/0438.Find-All-Anagrams-in-a-String/438. Find All Anagrams in a String.go
+++ leetcode/0438.Find-All-Anagrams-in-a-String/438. Find All Anagrams in a String.go
@@ -3,31 +3,43 @@ package leetcode
 func findAnagrams(s string, p string) []int {
 	var freq [256]int
 	result := []int{}
+
+	// 特殊情况处理
 	if len(s) == 0 || len(s) < len(p) {
 		return result
 	}
+
+	// 初始化频次数组
 	for i := 0; i < len(p); i++ {
 		freq[p[i]-'a']++
 	}
+
 	left, right, count := 0, 0, len(p)
 
+	// 遍历 s 字符串
 	for right < len(s) {
+		// 右侧字符在 p 中存在，count 减一
 		if freq[s[right]-'a'] >= 1 {
 			count--
 		}
 		freq[s[right]-'a']--
 		right++
+
+		// 当前窗口中的字符都匹配完成
 		if count == 0 {
 			result = append(result, left)
 		}
+
+		// 窗口长度达到 len(p)，移动左侧
 		if right-left == len(p) {
+			// 左侧字符是 p 中的有效字符，count 加一
 			if freq[s[left]-'a'] >= 0 {
 				count++
 			}
 			freq[s[left]-'a']++
 			left++
 		}
-
 	}
+
 	return result
 }
