4c64752a423c8d1ff0d9541d1b260dffe1b92f27
Yoke Cheng <18322535679@163.com>
2023-12-05 11:42:38 +0800

    [feat]labuladong 的算法笔记

--- leetcode/0019.Remove-Nth-Node-From-End-of-List/19. Remove Nth Node From End of List.go
+++ leetcode/0019.Remove-Nth-Node-From-End-of-List/19. Remove Nth Node From End of List.go
@@ -16,18 +16,31 @@ type ListNode = structures.ListNode
  */
 
 // 解法一
+// removeNthFromEnd 从链表末尾删除倒数第 N 个节点
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	// 使用虚拟头结点简化边界情况处理
 	dummyHead := &ListNode{Next: head}
+	// preSlow 用于记录要删除节点的前一个节点
+	// slow 和 fast 用于遍历链表
 	preSlow, slow, fast := dummyHead, head, head
+
+	// 使用快慢指针，fast 先走 N 步，然后 slow 和 fast 一起走
 	for fast != nil {
+		// 当 n 小于等于 0 时，preSlow 和 slow 开始同步移动
 		if n <= 0 {
 			preSlow = slow
 			slow = slow.Next
 		}
+		// 每遍历一个节点，n 减一
 		n--
+		// fast 继续向前移动
 		fast = fast.Next
 	}
+
+	// 删除倒数第 N 个节点
 	preSlow.Next = slow.Next
+
+	// 返回虚拟头结点的下一个节点，即处理后的链表头
 	return dummyHead.Next
 }
 
