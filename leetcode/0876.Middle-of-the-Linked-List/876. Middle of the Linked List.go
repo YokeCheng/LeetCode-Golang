44493ed273d27b2cf6011aa161af1b84b4e22800
Yoke Cheng <18322535679@163.com>
2023-12-07 16:58:38 +0800

    [feat]labuladong 的算法笔记

--- leetcode/0876.Middle-of-the-Linked-List/876. Middle of the Linked List.go
+++ leetcode/0876.Middle-of-the-Linked-List/876. Middle of the Linked List.go
@@ -15,19 +15,23 @@ type ListNode = structures.ListNode
  * }
  */
 
+// middleNode 返回链表的中间节点。如果链表有偶数个节点，则返回中间两个节点的第一个节点。
 func middleNode(head *ListNode) *ListNode {
-	if head == nil || head.Next == nil {
-		return head
-	}
-	p1 := head
-	p2 := head
-	for p2.Next != nil && p2.Next.Next != nil {
-		p1 = p1.Next
-		p2 = p2.Next.Next
+	// 使用两个指针，一个慢指针（slow）和一个快指针（fast），初始位置都在链表头部
+	slow, fast := head, head
+
+	// 遍历链表，快指针每次移动两步，慢指针每次移动一步
+	for fast != nil && fast.Next != nil {
+		// 快指针先移动一步
+		fast = fast.Next
+		// 如果快指针还能再移动一步，再移动一步
+		if fast != nil {
+			fast = fast.Next
+		}
+		// 慢指针每次移动一步
+		slow = slow.Next
 	}
 
-	if p2.Next != nil {
-		return p1.Next
-	}
-	return p1
+	// 当快指针到达链表末尾时，慢指针所在位置即为中间节点的位置
+	return slow
 }
