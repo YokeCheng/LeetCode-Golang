33b333cfaefcf70de9f0ffaa5ab9cb60e232cf4e
Yoke Cheng <18322535679@163.com>
2023-12-08 16:12:33 +0800

    [feat]labuladong 的算法笔记

--- leetcode/0141.Linked-List-Cycle/141. Linked List Cycle.go
+++ leetcode/0141.Linked-List-Cycle/141. Linked List Cycle.go
@@ -15,15 +15,26 @@ type ListNode = structures.ListNode
  * }
  */
 
+// hasCycle 检查链表是否有循环
 func hasCycle(head *ListNode) bool {
+	// 初始化两个指针，fast（快指针）和slow（慢指针），它们都从链表的头部开始
 	fast := head
 	slow := head
+
+	// 遍历链表，直到fast指针到达链表末尾或检测到循环
 	for fast != nil && fast.Next != nil {
+		// fast指针每次移动两步
 		fast = fast.Next.Next
+		// slow指针每次移动一步
 		slow = slow.Next
+
+		// 检查fast和slow指针是否相遇，表示存在循环
 		if fast == slow {
+			// 检测到循环，返回true
 			return true
 		}
 	}
+
+	// 如果循环完成而没有检测到循环，则返回false
 	return false
 }
