e875da1aaec314f81b46e42acc215b1640a5d01b
Yoke Cheng <18322535679@163.com>
2023-12-11 11:36:46 +0800

    [feat]labuladong 的算法笔记

--- leetcode/0142.Linked-List-Cycle-II/142. Linked List Cycle II.go
+++ leetcode/0142.Linked-List-Cycle-II/142. Linked List Cycle II.go
@@ -14,31 +14,43 @@ type ListNode = structures.ListNode
  *     Next *ListNode
  * }
  */
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
 func detectCycle(head *ListNode) *ListNode {
+	// 如果链表为空或者链表的头部为空，直接返回nil
 	if head == nil || head.Next == nil {
 		return nil
 	}
-	isCycle, slow := hasCycle142(head)
-	if !isCycle {
-		return nil
-	}
-	fast := head
-	for fast != slow {
-		fast = fast.Next
-		slow = slow.Next
-	}
-	return fast
-}
 
-func hasCycle142(head *ListNode) (bool, *ListNode) {
+	// 快指针从头节点开始，慢指针从头节点开始
 	fast := head
 	slow := head
+
+	// 当慢指针和快指针都不为空，且快指针的下一个节点不为空时，进行循环
 	for slow != nil && fast != nil && fast.Next != nil {
+		// 快指针移动两步
 		fast = fast.Next.Next
+		// 慢指针移动一步
 		slow = slow.Next
+		// 如果快指针追上了慢指针，说明链表中存在环
 		if fast == slow {
-			return true, slow
+			// 重置快指针到链表头部
+			fast = head
+			// 当快指针和慢指针相遇时，重新开始循环，直到快指针和慢指针相遇为止
+			for fast != slow {
+				fast = fast.Next
+				slow = slow.Next
+			}
+			// 当快指针和慢指针相遇时，返回相遇节点，这个节点是环的入口。
+			return fast
 		}
 	}
-	return false, nil
+
+	// 如果快指针追上慢指针的情况没有发生，说明链表中不存在环，返回nil。
+	return nil
 }
--- leetcode/0142.Linked-List-Cycle-II/142. Linked List Cycle II_test.go
+++ leetcode/0142.Linked-List-Cycle-II/142. Linked List Cycle II_test.go
@@ -1,6 +1,54 @@
 package leetcode
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/halfrost/LeetCode-Go/structures"
+)
+
+type question142 struct {
+	para142
+	ans142
+}
+
+// para 是参数
+// one 代表第一个参数
+type para142 struct {
+	one []int
+}
+
+// ans 是答案
+// one 代表第一个答案
+type ans142 struct {
+	one bool
+}
 
 func Test_Problem142(t *testing.T) {
+
+	qs := []question142{
+
+		{
+			para142{[]int{3, 2, 0, -4}},
+			ans142{false},
+		},
+
+		{
+			para142{[]int{1, 2}},
+			ans142{false},
+		},
+
+		{
+			para142{[]int{1}},
+			ans142{false},
+		},
+	}
+
+	fmt.Printf("------------------------Leetcode Problem 142------------------------\n")
+
+	for _, q := range qs {
+		_, p := q.ans142, q.para142
+		fmt.Printf("【input】:%v       【output】:%v\n", p, detectCycle(structures.Ints2List(p.one)))
+	}
+	fmt.Printf("\n\n\n")
 }
